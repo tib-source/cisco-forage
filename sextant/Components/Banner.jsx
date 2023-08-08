5f264392eec6ea9c21fffc4d5b43bf67c17763c4
Tibebe Demissie <tib.demissie@gmail.com>
2023-08-08 12:32:21 +0100

    Created a banner and an exhibit component

---
+++ sextant/Components/Banner.jsx
@@ -0,0 +1,7 @@
+export default function Banner() {
+	return (
+		<div className="app__banner">
+			<h1>Sexant</h1>
+		</div>
+	);
+}
---
+++ sextant/Components/MainBody.jsx
@@ -0,0 +1,3 @@
+export default function MainBody() {
+	return <div>MainBody</div>;
+}
